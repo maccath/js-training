7d29669ad610f7614ac43c9aadf9aad9d6918438
Katy Ereira <katye@deeson.co.uk>
2017-04-07 17:19:55 +0100

    Lesson 2

--- node-basics/program.js
+++ node-basics/program.js
@@ -1,1 +1,9 @@
-console.log('HELLO WORLD');
+var total = 0;
+var args = process.argv.slice(2);
+
+
+for (var i = 0; i < args.length; i++) {
+  total = total + Number(args[i]);
+}
+
+console.log(total);
