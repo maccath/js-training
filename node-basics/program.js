7af2e2a82ffd4e03b66a0638bf705b5c14e9880a
Katy Ereira <katye@deeson.co.uk>
2017-04-07 17:28:58 +0100

    Challenge 4

--- node-basics/program.js
+++ node-basics/program.js
@@ -1,8 +1,7 @@
 var fs = require('fs');
 
-var buffer = fs.readFileSync(process.argv[2]);
-var string = buffer.toString();
+fs.readFile(process.argv[2], 'utf8', function (err, data) {
+  var newlines = data.split('\n').length - 1;
+  console.log(newlines);
+});
 
-var newlines = string.split('\n').length - 1;
-
-console.log(newlines);
